6a75071200183f812c8108ace4404a69b190c9bb
Deep-Rewale <67994719+Deep-Rewale@users.noreply.github.com>
2025-07-13 17:23:13 +0530

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -63,9 +63,9 @@ show_header() {
     echo "                                                                                            "
     
     echo -e "${YELLOW}           🚀 Gensyn RL-Swarm Launcher Made By Deep 🚀${NC}"
-    echo -e "${YELLOW}              GitHub: https://github.com/Deep-Rewale${NC}"
-    echo -e "${YELLOW}              Telegram: https://t.me/Deeprewale${NC}"
-     echo -e "${YELLOW}             X ACCOUNT: https://x.com/deep_rewale28${NC}"
+    echo -e "${YELLOW}            💻 GitHub: https://github.com/Deep-Rewale${NC}"
+    echo -e "${YELLOW}            💬Telegram: https://t.me/Deeprewale${NC}"
+     echo -e "${YELLOW}           💡 X ACCOUNT: https://x.com/deep_rewale28${NC}"
     echo -e "${GREEN}===============================================================================${NC}"
 }
 
